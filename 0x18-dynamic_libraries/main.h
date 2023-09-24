a56d4193741c951762ccc07834295d6d1cfef317
Emad <49229264+alsabahiemad@users.noreply.github.com>
2023-09-24 17:09:24 +0700

    Add files via upload

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <stdlib.h>
 
 int _putchar(char c);
@@ -25,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif
